2cde0efa3364f8bdc127d88bcadb7a2c1677112a
Victorino <victorino_s@outlook.com>
2017-06-23 21:49:55 +0200

    Bug du saut

--- unity_cod_zombies_like/Assets/PlayerController.cs
+++ unity_cod_zombies_like/Assets/PlayerController.cs
@@ -10,13 +10,11 @@ public class PlayerController : MonoBehaviour {
 
     public bool isGrounded = true;
     bool isMoving = false;
-    bool isJumping = false;
 
     public float speed;
     public float sensitivity;
     public float viewRange;
-    public float jumpSpeed;
-    public float jumpDistance;
+    public float jumpForce;
 
     public bool useCustomGravity;
     public float customGravity;
@@ -31,10 +29,6 @@ public class PlayerController : MonoBehaviour {
         set { isMoving = value; }
     }
 
-    public bool IsJumping
-    {
-        get { return isJumping; }
-    }
 	// Use this for initialization
 	void Start () {
         _lookComponent = GetComponentInChildren<PlayerLook>();
@@ -43,45 +37,19 @@ public class PlayerController : MonoBehaviour {
         speed = speed > 0 ? speed : 5f;
         sensitivity = sensitivity > 0 ? sensitivity : 3f;
         viewRange = viewRange > 0 ? viewRange : 60f;
-        jumpSpeed = jumpSpeed > 0 ? jumpSpeed : 6f;
-        jumpDistance = jumpDistance > 0 ? jumpDistance : 2f;
+        jumpForce = jumpForce > 0 ? jumpForce : 6f;
 
         _lookComponent.Initialize(this);
         _moveComponent.Initialize(this);
-
-        if(!useCustomGravity)
-        {
-            GetComponent<Rigidbody>().useGravity = true;
-        }
-        else
-        {
-            if(customGravity == 0f)
-            {
-                customGravity = 9.81f;
-            }
-        }
-        
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        /*
-        if (!isGrounded)
-        {
-            if(useCustomGravity)
-            {
-                GetComponent<Rigidbody>().AddForce( ((-1f) * Vector3.up) * customGravity * GetComponent<Rigidbody>().mass);
-            }
-            
-            
-        }
-        */
         Ray ray = new Ray(transform.position, new Vector3(0f, -1f, 0f));
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, 1.1f))
         {
             isGrounded = true;
-            isJumping = false;
         }
         else
         {
@@ -92,11 +60,5 @@ public class PlayerController : MonoBehaviour {
     public void UpdateRotation(float yaw)
     {
         transform.localEulerAngles = new Vector3(0f, yaw, 0f); 
-    }
-
-    public void EnterJump()
-    {
-        //isGrounded = false;
-        isJumping = true;
     }
 }
--- unity_cod_zombies_like/Assets/PlayerMove.cs
+++ unity_cod_zombies_like/Assets/PlayerMove.cs
@@ -3,96 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //[RequireComponent(typeof(CharacterController))]
-public class PlayerMove : MonoBehaviour {
+public class PlayerMove : MonoBehaviour
+{
 
     PlayerController controller;
-    float targetHeigth;
-    bool targetHeigthReached = false;
+    float secondsLeft;
+    bool isJumping = false;
     // Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    void Start()
+    {
 
-        bool movingLeft = Input.GetAxis("Horizontal") < 0f;
-        bool movingRight = Input.GetAxis("Horizontal") > 0f;
-        bool movingForward = Input.GetAxis("Vertical") > 0f;
-        bool movingBackward = Input.GetAxis("Vertical") < 0f;
-        bool jump = Input.GetAxis("Jump") != 0f;
+    }
 
-        if(movingForward)
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump") && controller.IsGrounded)
         {
-            controller.IsMoving = true;
-            GetComponent<Rigidbody>().velocity = controller.speed * transform.forward ;
+            GetComponent<Rigidbody>().AddForce(controller.jumpForce * Vector3.up, ForceMode.Impulse);
         }
-        else if(movingBackward)
+
+
+        
+        if (Input.GetButton("Forward"))
         {
             controller.IsMoving = true;
-            GetComponent<Rigidbody>().velocity = controller.speed * transform.forward * (-1f);
+            GetComponent<Rigidbody>().AddForce(controller.speed * transform.forward, ForceMode.Acceleration);
+
         }
-        else if (movingRight)
+        /*
+         * Cette partie bug car le GetButtonDown est bloquant et ducoup l'objet redescent par a coups lors des sauts (atterissage)
+         * De meme lorsque que je faisais : bool isMovingForward = Input.GetAxis("Vertical") > 0;
+         * 
+        if (Input.GetButtonDown("Forward"))
         {
             controller.IsMoving = true;
-            GetComponent<Rigidbody>().velocity = controller.speed * transform.right;
+            GetComponent<Rigidbody>().AddForce(controller.speed * transform.forward, ForceMode.Acceleration);
+
         }
-        else if (movingLeft)
+        else if (Input.GetButtonDown("Backward"))
         {
             controller.IsMoving = true;
-            GetComponent<Rigidbody>().velocity = controller.speed * transform.right * (-1f);
+            GetComponent<Rigidbody>().AddForce(controller.speed * transform.forward * (-1f), ForceMode.Force);
+        }
+        else if (Input.GetButtonDown("Right"))
+        {
+            controller.IsMoving = true;
+            GetComponent<Rigidbody>().AddForce(controller.speed * transform.right, ForceMode.Force);
+        }
+        else if (Input.GetButtonDown("Left"))
+        {
+            controller.IsMoving = true;
+            GetComponent<Rigidbody>().AddForce(controller.speed * transform.right * (-1f), ForceMode.Force);
         }
         else
         {
             controller.IsMoving = false;
             GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
+        */
 
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            if(controller.IsGrounded)
-            {
-                controller.EnterJump();
-                targetHeigthReached = false;
-                targetHeigth = transform.position.y + controller.jumpDistance;
-                Elevate();
-            }
-        }
-
-        if(controller.IsJumping)
-        {
-            if(!targetHeigthReached)
-            {
-                if(transform.position.y >= targetHeigth)
-                {
-                    targetHeigthReached = true;
-                }
-                Elevate();
-            }
-            else
-            {
-                Fall();
-            }
-        }
-       
     }
 
     public void Initialize(PlayerController controller)
     {
         this.controller = controller;
     }
-
-    void Elevate()
-    {
-        GetComponent<Rigidbody>().velocity += controller.jumpSpeed * Vector3.up;
-    }
-
-    void Fall()
-    {
-        if(controller.useCustomGravity)
-        {
-            GetComponent<Rigidbody>().velocity = controller.customGravity * (-1f) * Vector3.up;
-        }
-    }
-
 }
