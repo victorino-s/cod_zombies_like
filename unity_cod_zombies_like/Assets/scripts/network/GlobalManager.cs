369813f71ff05de1c09fb3f23e4f5ffb33252345
Victorino <victorino_s@outlook.com>
2017-06-29 16:56:02 +0200

    Work on global networking

---
+++ unity_cod_zombies_like/Assets/scripts/network/GlobalManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GlobalManager : NetworkManager {
+
+    public Material[] player_materials = new Material[4];
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        var player = (GameObject)GameObject.Instantiate(playerPrefab, GetStartPosition().position, Quaternion.identity);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+        switch(numPlayers)
+        {
+            case 1:
+                player.GetComponent<MeshRenderer>().material = player_materials[0];
+                break;
+            case 2:
+                player.GetComponent<MeshRenderer>().material = player_materials[1];
+                break;
+            case 3:
+                player.GetComponent<MeshRenderer>().material = player_materials[2];
+                break;
+            case 4:
+                player.GetComponent<MeshRenderer>().material = player_materials[3];
+                break;
+            default:
+                player.GetComponent<MeshRenderer>().material = player_materials[0];
+                break;
+        }
+    }
+}
